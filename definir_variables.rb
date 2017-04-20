c64f38524fd69703e357ff5a6d23f02fe5e8b4ae
codeamac12 <codeamac12@gmail.com>
2017-04-20 18:30:19 -0500

    definir variables version 1

---
+++ definir_variables.rb
@@ -0,0 +1,26 @@
+#Define una variable tipo entero.
+p edad = 29
+#Define dos variables tipo flotante.
+p decimal = 23.10
+p precio_venta = 19.99
+#Define tr
+es variables tipo boolean
+p igual_que = 35 == 5 * 7
+#=> true
+p mayor_que = 50 < 20
+#=> false
+p diferente_que = 30 != 30
+#=> false
+#Define una variable y asígnale un arreglo que tenga valores tipo string.
+p profesion = ["Diseño Gráfico"]
+#Define una variable y asígnale un hash con tres elementos tipo entero.
+p niveles = { 1 => "principiante", 2 => "intermedio", 3 => "avanzado" }
+
+
+
+
+
+
+
+
+
